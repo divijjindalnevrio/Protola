d443d52ef9886087be25832d596f7ecf67f7d49c
Amit7M <112542159+Amit7M@users.noreply.github.com>
2024-01-19 13:55:24 +0530

    made two individual function for basin and counter generation.

--- Assets/Scripts/BasinMovement.cs
+++ Assets/Scripts/BasinMovement.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class BasinMovement : MonoBehaviour
 {
     [SerializeField] private GameObject basin;
+
     [SerializeField] private Camera mainCam;
     [SerializeField] private float speed = 1;
+    [SerializeField] private float Counterspeed = 1;
+    private GameObject _instanciateBasin;
+    private GameObject _instanciateCounter;
 
     public static bool _isSelected;
     public GameObject currentBasin;
-    [SerializeField] private GameObject _instanciateBasin;
+    public GameObject currentCounter;
+
     public LayerMask layerMask;
-    private bool isBasinHit = false;
-    private bool isBasinSelect = false;
+    public LayerMask CounterlayerMask;
+
+    private bool isBasinInstanciate = false;
+    private bool isCounterInstanciate = false;
 
     private Color cubeMat;
     private void Start()
@@ -24,48 +31,8 @@ public class BasinMovement : MonoBehaviour
 
     void Update()
     {
-        if(currentBasin != null && Input.touchCount == 1)
-        {
-            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, layerMask))
-            {
-                _isSelected = true;
-                Debug.Log("hit counter layer : ");
-                if(raycastHit.collider.tag == "Basin" && isBasinHit == false)
-                {
-                    Debug.Log("yes it is s basin");
-                    _instanciateBasin = Instantiate(currentBasin, currentBasin.transform.position, Quaternion.identity);
-                    _instanciateBasin.transform.parent = currentBasin.transform.parent;
-                    currentBasin = _instanciateBasin;
-                    isBasinHit = true;
-                }
-
-                if(_instanciateBasin != null && raycastHit.collider.tag == "Counter")
-                {
-                        Debug.Log("counter is selecting now ");
-                        Vector3 targetPosition = new Vector3(raycastHit.point.x, currentBasin.transform.position.y, raycastHit.point.z);
-                        _instanciateBasin.transform.position = Vector3.Lerp(currentBasin.transform.position, targetPosition, Time.deltaTime * speed);
-
-                   
-                }
-               
-            }
-            
-        }
-
-
-        //if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
-        //{
-
-        //    _instanciateBasin = Instantiate(currentBasin, currentBasin.transform.position, Quaternion.identity);
-        //    _instanciateBasin.transform.parent = currentBasin.transform.parent;
-
-        //}
-
-        //if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
-        //{
-        //   // _isSelected = true;
-        //}
+        // BasinMovementAndGerenartion();
+        CounterMovementAndGerenartion();
 
         if (Input.touchCount >= 1 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
@@ -74,9 +41,80 @@ public class BasinMovement : MonoBehaviour
 
     }
 
+    private void BasinMovementAndGerenartion()
+    {
+        if (currentBasin != null && Input.touchCount == 1)
+        {
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, layerMask))
+            {
+                _isSelected = true;
+                if (raycastHit.collider.tag == "Basin" && isBasinInstanciate == false)
+                {
+                    BasinInstanciate();
+                }
+
+                if (_instanciateBasin != null && raycastHit.collider.tag == "Counter")
+                {
+                    Vector3 targetPosition = new Vector3(raycastHit.point.x, currentBasin.transform.position.y, raycastHit.point.z);
+                    _instanciateBasin.transform.position = Vector3.Lerp(currentBasin.transform.position, targetPosition, Time.deltaTime * speed);
+                }
+
+            }
+
+        }
+
+    }
+
+
+    private void BasinInstanciate()
+    {
+        _instanciateBasin = Instantiate(currentBasin, currentBasin.transform.position, Quaternion.identity);
+        _instanciateBasin.transform.parent = currentBasin.transform.parent;
+        currentBasin = _instanciateBasin;
+        isBasinInstanciate = true;
+    }
+
+
+
+    private void CounterMovementAndGerenartion()
+    {
+        if (Input.touchCount == 1)
+        {
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, CounterlayerMask))
+            {
+                _isSelected = true;
+                if (raycastHit.collider.tag == "Counter" && isCounterInstanciate == false)
+                {
+                    CounterInstanciate();
+                }
+
+                if (_instanciateCounter != null && raycastHit.collider.tag == "Counter")
+                {
+                    Vector3 targetPosition = new Vector3(raycastHit.point.x, currentCounter.transform.position.y, raycastHit.point.z);
+                    _instanciateCounter.transform.position = Vector3.Lerp(currentCounter.transform.position, targetPosition, Time.deltaTime * Counterspeed);
+                }
+
+            }
+
+        }
+
+    }
+
+    private void CounterInstanciate()
+    {
+        _instanciateCounter = Instantiate(currentCounter, currentCounter.transform.position, Quaternion.identity);
+        _instanciateCounter.transform.parent = currentCounter.transform.parent;
+        currentCounter = _instanciateCounter;
+        isCounterInstanciate = true;
+    }
+
+
+
     public void BasinGererator()
     {
-        if(this.gameObject.transform.childCount <= 1)
+        if(this.gameObject.transform.childCount < 1)
         {
             currentBasin = Instantiate(basin, transform.position, Quaternion.identity);
             currentBasin.transform.parent = this.gameObject.transform;

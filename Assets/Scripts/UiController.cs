25d987aecb2cbce5c140b61469e99d79326527c5
Amit7M <112542159+Amit7M@users.noreply.github.com>
2024-01-20 22:25:41 +0530

    fix,  moving navigation while sliding the slider for scaling counter

---
+++ Assets/Scripts/UiController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UiController : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsPointerOverUIObject())
+        {
+            BasinMovement._isSelected = true;
+        }
+    }
+    private void OnMouseEnter()
+    {
+       // Debug.Log("there you go we onmouseEnter");
+    }
+
+    public bool IsPointerOverUIObject()      //Called to check if the pointer is over a ui object
+    {
+        bool value = false;
+        for (int i = 0; i < Input.touches.Length; i++)
+        {
+            int Index = i;
+            value = EventSystem.current.IsPointerOverGameObject(Input.GetTouch(Index).fingerId);
+            if (value)
+                break;
+        }
+        return value;
+    }
+}
+

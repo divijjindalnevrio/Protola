93a02551e98076a9a51f9ccc066f5a68c39c36db
Amit7M <112542159+Amit7M@users.noreply.github.com>
2024-01-21 18:25:26 +0530

    added rotating ui buttons in world space that can be change orientations based on main camera

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private Camera Maincamera;
+    void Start()
+    {
+        Maincamera = Camera.main;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+
+        transform.LookAt(transform.position + Maincamera.transform.rotation * Vector3.back, Maincamera.transform.rotation * Vector3.up);
+    }
+}

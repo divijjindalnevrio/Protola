7f178f9138f5f944d526ef3cc515cc630f64c507
Amit7M <112542159+Amit7M@users.noreply.github.com>
2024-02-02 14:31:25 +0530

    Added, switch functionality clicking on scale buttons through script.

---
+++ Assets/Scripts/Ui/MainUiController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUiController : MonoBehaviour
+{
+    private UiModel uiModel;
+    private GameObject sliders;
+
+    void Start()
+    {
+        // assign ui elements to dictonary!
+        uiModel = GetComponent<UiModel>();
+        uiModel.AddUiElementsToDictonary();
+        sliders = uiModel.UiBuoonsAndElementsDict["SurfaceSizeContent"].transform.Find("Sliders").gameObject;
+        InitialButtoUiState();
+
+        Debug.Log("AddUiElementsToDictonary : " + uiModel.UiBuoonsAndElementsDict["SurfaceSizeAndColorButtonsTop"].name  + uiModel.UiBuoonsAndElementsDict.Count);
+    }
+
+    public void InitialButtoUiState()
+    {
+        uiModel.UiBuoonsAndElementsDict["SurfaceSizeAndColorButtonsTop"].SetActive(true);
+        uiModel.UiBuoonsAndElementsDict["SurfaceSizeContent"].SetActive(true);
+        uiModel.UiBuoonsAndElementsDict["Background"].SetActive(true);
+        uiModel.UiBuoonsAndElementsDict["SurfaceSizeContent"].transform.Find("Sliders").transform.Find("Width").gameObject.SetActive(true);
+    }
+
+    public void SettingSliderToActive(string sliderName)
+    {
+        switch(sliderName)
+        {
+            case "Depth":
+                SetAllSlidersToFalse();
+                uiModel.UiBuoonsAndElementsDict["SurfaceSizeContent"].transform.Find("Sliders").transform.Find("Depth").gameObject.SetActive(true);
+                break;
+
+            case "Width":
+                SetAllSlidersToFalse();
+                uiModel.UiBuoonsAndElementsDict["SurfaceSizeContent"].transform.Find("Sliders").transform.Find("Width").gameObject.SetActive(true);
+                break;
+
+            case "Thickness":
+                SetAllSlidersToFalse();
+                uiModel.UiBuoonsAndElementsDict["SurfaceSizeContent"].transform.Find("Sliders").transform.Find("Thickness").gameObject.SetActive(true);
+                break;
+        }
+    }
+
+    private void SetAllSlidersToFalse()
+    {
+        foreach(Transform child in sliders.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Ui/UiModel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiModel : MonoBehaviour
+{
+  
+    public List<GameObject> UiBuoonsAndElementsList = new List<GameObject>();
+    public Dictionary<string, GameObject> UiBuoonsAndElementsDict = new Dictionary<string, GameObject>();
+
+    public void AddUiElementsToDictonary()
+    {
+        foreach (GameObject listitem in UiBuoonsAndElementsList)
+        {
+            UiBuoonsAndElementsDict.Add(listitem.name, listitem);
+            listitem.SetActive(false);
+        }
+    }
+}

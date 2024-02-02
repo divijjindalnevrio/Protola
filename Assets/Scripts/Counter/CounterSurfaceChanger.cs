06e37ea59978c8cafbcf6871fc4e09e578eb79e8
Amit7M <112542159+Amit7M@users.noreply.github.com>
2024-02-02 19:00:26 +0530

    Added Color to counter

---
+++ Assets/Scripts/Counter/CounterSurfaceChanger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CounterSurfaceChanger : MonoBehaviour
+{
+    [SerializeField] private List<Material> counterMat = new List<Material>();
+    [SerializeField] private BasinMovement basinMovement;
+
+    public List<Color> colors = new List<Color>();
+
+    void Start()
+    {
+        
+    }
+
+    public void ChangingCounterSurface(int material)
+    {
+        basinMovement.currentCounter.transform.Find("Counter").GetComponent<MeshRenderer>().material = counterMat[material];
+    }
+    public void ChangingCounterSurfaceColor(int color)
+    {
+       basinMovement.currentCounter.transform.Find("Counter").GetComponent<MeshRenderer>().material.color = colors[color];
+    }
+}

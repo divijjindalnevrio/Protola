ba84d87a1845dac6c9ef8acc54b1155a92595e00
Amit7M <112542159+Amit7M@users.noreply.github.com>
2024-03-12 12:29:22 +0530

    set the text field center to z axis

--- Assets/Scripts/MeasurementLineController.cs
+++ Assets/Scripts/MeasurementLineController.cs
@@ -30,24 +30,30 @@ public class MeasurementLineController : MonoBehaviour
         if(basinMovement.selectedObject == SelectedObject.basin)
         {
             LineLength.Clear();
-            LineLength.Add(Mathf.Abs((measurementLines[0].GetPosition(0).x - measurementLines[0].GetPosition(1).x) * 100));
-            LineLength.Add(Mathf.Abs((measurementLines[1].GetPosition(0).x - measurementLines[1].GetPosition(1).x) * 100));
-            LineLength.Add(Mathf.Abs((measurementLines[2].GetPosition(0).z - measurementLines[2].GetPosition(1).z) * 100));
-            LineLength.Add(Mathf.Abs((measurementLines[3].GetPosition(0).z - measurementLines[3].GetPosition(1).z) * 100));
-            SettingTextFeildPosition();
+            float measurementLinesXValOne = measurementLines[0].GetPosition(0).x - measurementLines[0].GetPosition(1).x;
+            float measurementLinesXValTwo = measurementLines[1].GetPosition(0).x - measurementLines[1].GetPosition(1).x;
+            float measurementLinesZValOne = measurementLines[2].GetPosition(0).z - measurementLines[2].GetPosition(1).z;
+            float measurementLinesZValTwo = measurementLines[3].GetPosition(0).z - measurementLines[3].GetPosition(1).z;
+
+            LineLength.Add(Mathf.Abs(measurementLinesXValOne * 100));
+            LineLength.Add(Mathf.Abs(measurementLinesXValTwo * 100));
+            LineLength.Add(Mathf.Abs(measurementLinesZValOne * 100));
+            LineLength.Add(Mathf.Abs(measurementLinesZValTwo * 100));
+            SettingTextFeildPosition(measurementLinesXValOne, measurementLinesXValTwo, measurementLinesZValOne, measurementLinesZValTwo);
             AssignMeasurementLineLengthToFeild();
-            //measurementLinesInputFeilds[0].transform.GetChild(0).transform.GetChild(0).transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "7077";
+           
         }
 
     }
 
-    private void SettingTextFeildPosition()
+    private void SettingTextFeildPosition(float XValOne, float XValTwo, float ZValOne, float ZValTwo)
     {
         MeasurementLineUi.SetActive(true);
-        measurementLinesInputFeilds[3].transform.position = basinDashLine.basinEdgePoints[0] + new Vector3(0, 0.2f, 0.4f);
-        measurementLinesInputFeilds[2].transform.position = basinDashLine.basinEdgePoints[1] + new Vector3(0, 0.2f, 0);
-        measurementLinesInputFeilds[1].transform.position = basinDashLine.basinEdgePoints[2] + new Vector3(0, 0.2f, 0);
-        measurementLinesInputFeilds[0].transform.position = basinDashLine.basinEdgePoints[3] + new Vector3(0, 0.2f, 0);
+        measurementLinesInputFeilds[3].transform.position = basinDashLine.basinEdgePoints[0] + new Vector3(0, 0.2f, ZValOne/2);      // common in z axis
+        measurementLinesInputFeilds[2].transform.position = basinDashLine.basinEdgePoints[1] + new Vector3(0, 0.2f, ZValTwo/2);
+
+        measurementLinesInputFeilds[1].transform.position = basinDashLine.basinEdgePoints[2] + new Vector3(XValTwo/2 , 0.2f, 0);         // common in x axis
+        measurementLinesInputFeilds[0].transform.position = basinDashLine.basinEdgePoints[3] + new Vector3(XValOne/2, 0.2f, 0);
     }
 
     private void AssignMeasurementLineLengthToFeild()

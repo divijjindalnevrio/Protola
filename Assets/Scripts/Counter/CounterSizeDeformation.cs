f7d8880cd3f7a551c214223fe3dd0a3dc839b99c
Amit7M <112542159+Amit7M@users.noreply.github.com>
2024-03-17 20:05:08 +0530

    working on counter deformation

--- Assets/Scripts/Counter/CounterSizeDeformation.cs
+++ Assets/Scripts/Counter/CounterSizeDeformation.cs
@@ -8,10 +8,10 @@ public class CounterSizeDeformation : MonoBehaviour
     [SerializeField] private Slider widthSlider;
     [SerializeField] private Slider thicknessSlider;
     [SerializeField] private Slider depthSlider;
-    
-    private float width;
-    private float thickness;
-    private float depth;
+
+    [SerializeField] private float width;
+    [SerializeField] private float thickness;
+    [SerializeField] private float depth;
     private CounterGenerator counterGenerator;
     [SerializeField] private Transform currentCounter;
     [SerializeField] private BasinMovement basinMovement;
@@ -31,14 +31,31 @@ public class CounterSizeDeformation : MonoBehaviour
     {
         if(e == SelectedObject.counter)
         {
-            GettingCurrentCounter();
+            currentCounter = basinMovement.currentCounter.transform;
+            Debug.Log("CURRENT COUNTER NAME : " + currentCounter.transform.name + currentCounter.transform.Find("Counter").transform.localScale);
+            SetTheSizeValueOfCurrentCounter();
         }
+
+
     }
 
     private void Update()
     {
+       // Debug.Log("SetTheSizeValueOfCurrentCounter : counterWidth : " + currentCounter.transform.localScale);
+        
         //ChangingSizeOfCounter();
 
+    }
+
+
+    public void SetTheSizeValueOfCurrentCounter()
+    {
+        Debug.Log("CURRENT COUNTER NAME PROBLEM ");
+        Debug.Log("SetTheSizeValueOfCurrentCounter : counterWidth :");
+        depthSlider.value = currentCounter.transform.Find("Counter").transform.localScale.z;
+        //widthSlider.value = currentCounter.transform.Find("Counter").transform.localScale.x;
+        //thicknessSlider.value = currentCounter.transform.localPosition.y;
+        
     }
 
     public void ChangingSizeOfCounter()
@@ -49,12 +66,12 @@ public class CounterSizeDeformation : MonoBehaviour
 
        currentCounter.transform.Find("Counter").transform.localScale = new Vector3(width, currentCounter.transform.Find("Counter").transform.localScale.y, depth);
        currentCounter.position = new Vector3(currentCounter.position.x, thickness, currentCounter.position.z);
-       currentCounterPosition = currentCounter.transform.localPosition;
+        currentCounterPosition = currentCounter.transform.localPosition;
     }
 
     private void GettingCurrentCounter()
     {
-        currentCounter = basinMovement.currentCounter.transform;
+        
     }
 
     

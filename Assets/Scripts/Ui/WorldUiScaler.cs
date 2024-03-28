c176eef227cf87c8031a99f847bcf08c677225ab
Amit7M <112542159+Amit7M@users.noreply.github.com>
2024-03-28 17:28:24 +0530

    Added circular path to basin

--- Assets/Scripts/Ui/WorldUiScaler.cs
+++ Assets/Scripts/Ui/WorldUiScaler.cs
@@ -8,17 +8,31 @@ public class WorldUiScaler : MonoBehaviour
     [SerializeField] private Camera mainCamera;
     [SerializeField] private GameObject worldUiObject;
     private float dist;
+    [SerializeField] private Vector3 InitialScale;
 
-     void Update()
+    private void Start()
     {
-        dist = Vector3.Distance(worldUiObject.transform.position, mainCamera.transform.position);
-        Canvas worldUiCanvas = worldUiObject.transform.GetChild(0).transform.GetChild(0).GetComponent<Canvas>();
-        Vector3 vect = (Vector3.one * dist / 3)/50;
-        GetAllTheWorldUiCanvas(vect);
-       // worldUiCanvas.transform.localScale = vect;
-        Debug.Log("Distance between camera and worlduiObject " + dist + vect);
+        InitialScale = CalculatingSizeOfWorldUiCanvas();
     }
 
+    void Update()
+    {
+        CalculatingSizeOfWorldUiCanvas();
+    }
+
+    private Vector3 CalculatingSizeOfWorldUiCanvas()
+    {
+        
+        dist = Vector3.Distance(worldUiObject.transform.position, mainCamera.transform.position);
+        Canvas worldUiCanvas = worldUiObject.transform.GetChild(0).transform.GetChild(0).GetComponent<Canvas>();
+        Vector3 vect = (Vector3.one * dist / 3) / 50;
+        if(vect.x >= InitialScale.x)
+        {
+            GetAllTheWorldUiCanvas(vect);
+        }
+        
+        return vect;
+    }
 
     void GetAllTheWorldUiCanvas(Vector3 vect)
     {

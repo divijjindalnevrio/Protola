aa85029a72a04b819cc31b1b42d5d856aea38d9d
Amit7M <112542159+Amit7M@users.noreply.github.com>
2024-04-23 18:17:23 +0530

    refactor the "counterSo".

--- Assets/Scripts/ScriptableObjectScripts/CounterTypeSO.cs
+++ Assets/Scripts/ScriptableObjectScripts/CounterTypeSO.cs
@@ -11,49 +11,5 @@ public class CounterTypeSO : ScriptableObject
     public CounterModel counterModel;
     public GameObject CurrenetCounter;
     public List<GameObject> counterType = new List<GameObject>();
-    private string str;
-
-
-    public void MakeNewObject()
-    {
-        counterModel = new CounterModel();
-    }
-
-    public void SettingCounterSize(float width, float thickness, float depth)
-    {
-        counterModel.width = width;
-        counterModel.depth = depth;
-        counterModel.thickness = thickness;
-    }
-
-    public void SetCounterRotationAndPosition(Vector3 rotation, Vector3 position)
-    {
-        counterModel.rotaton = rotation;
-        counterModel.position = position;
-    }
-
-    public void SettingTexture(string texture, string alphaTexture)
-    {
-        counterModel.texture = texture;
-        counterModel.alphaTexture = alphaTexture;
-    }
-    public void SetTheColor(string hexCode)
-    {
-        counterModel.colourHexCode = hexCode.Insert(0, "#");
-    }
-
-    public void SetPlywoodLength(List<GameObject> plywoods, float length)
-    {
-        counterModel.plywoodTextfield.Clear();
-        for (int i = 0; i < plywoods.Count; i++)
-        {
-            counterModel.plywoodTextfield.Add(length);
-        }
-    }
-
-    public void AssignBasinList(List<BasinModel> basinModels)
-    {
-       // counterModel.allbasins.Clear();
-        counterModel.allbasins = basinModels;
-    }
+   
 }   
